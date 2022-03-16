cd783ce10790c4f0c1254d9878660961af2863ea
shudson5523 <shudson5523@protonmail.com>
2022-03-15 20:13:30 -0400

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Scott Hudson on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipesListView()
+        }
+    }
+}
---
+++ Cookcademy/Views/RecipesListView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Cookcademy
+//
+//  Created by Scott Hudson on 3/14/22.
+//
+
+import SwiftUI
+
+struct RecipesListView: View {
+    @StateObject var recipeData = RecipeData()
+    
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    var body: some View {
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+            }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
+        }
+        .navigationTitle(navigationTitle)
+    }
+}
+
+extension RecipesListView {
+    var recipes: [Recipe] {
+        recipeData.recipes
+    }
+    
+    var navigationTitle: String {
+        "All Recipes"
+    }
+}
+
+struct RecipeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            RecipesListView()
+        }
+    }
+}
